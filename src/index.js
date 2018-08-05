d1e91a4a1411e27771d91abae6f65f3c1250e078
Patrick Pissurno <contato@patrickpissurno.com.br>
2018-08-05 13:10:08 -0300

    Gracefully handle piping errors

--- src/index.js
+++ src/index.js
@@ -13,8 +13,21 @@ module.exports = fp(function (fastify, opts, done) {
     fastify.addHook('onRequest', (req, res, next) => {
         if(req.headers.host != null && req.headers.host.indexOf(subdomain) !== -1)
         {
-            let target = { url: req.url, baseUrl: opts.upstream };
-            req.pipe(request(target)).pipe(res);
+            let target = { url: req.url, baseUrl: opts.upstream, headers: req.headers };
+            try
+            {
+                req.pipe(request(target)).pipe(res);
+            }
+            catch(ex){
+                res.statusCode = 500;
+                res.setHeader('Content-Type', 'application/json');
+                res.end(JSON.stringify({
+                    "statusCode": 500,
+                    "error": "Internal Server Error",
+                    "message": "Cannot proxy this request to the origin server"
+                }));
+                console.error(ex);
+            }
         }
         else
             next();
